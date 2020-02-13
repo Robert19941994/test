7f5ad7f23324ef3d396ceb5a060e0a0cafd81eb0
Robert <rob.markaryan@yandex.ru>
2020-02-13 17:14:28 +0300

    Initial commit

---
+++ 1_js.js
@@ -0,0 +1,13 @@
+let person = {
+    name:"John",
+    age:25,
+    isMarried: false
+};
+
+console.log(person.name);
+
+//let answer = prompt("Are you 18+","Yes");
+
+console.log(typeof(5+"sdfsd"));
+
+
